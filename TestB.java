efbea6890e234916dc18d8aed688217bacf07e22
liujixiao <liujixiao@sina.cn>
2017-04-16 21:33:54 +0800

    Edit from local

--- TestB.java
+++ TestB.java
@@ -1,6 +1,10 @@
 public class TestB{
+	public void sayHello(){
+		System.out.println("Hello World!");
+	}
+
 	public static void main(String[] args){
-		System.out.println("Hello TestB!");
+		System.out.println("Hello TestB!,Edit from local!");
 		System.out.println("GoodBye TestB!");
 	}
 }

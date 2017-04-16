24ebd474c59ce5fd3e84e31099af925101a49129
liujixiao <liujixiao@sina.cn>
2017-04-16 21:58:19 +0800

    edit from release

--- TestA.java
+++ TestA.java
@@ -1,6 +1,7 @@
 public class TestA{
 	public static void main(String[] args){
 		System.out.println("Hello TestA,GoodBye TestA!");
-		System.out.println("Edit from release!");
+		//Edit from release branch
+		System.out.println("Edit from release branch!");
 	}
 }

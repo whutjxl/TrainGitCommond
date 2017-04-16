e3960ab9a4e35121c8c6f0dd2db646e5aa9aec6f
liujixiao <liujixiao@sina.cn>
2017-04-16 21:57:05 +0800

    edit from master

--- TestA.java
+++ TestA.java
@@ -1,6 +1,7 @@
 public class TestA{
 	public static void main(String[] args){
-		System.out.println("Hello TestA,GoodBye TestA!");
+		//Edit from master branch
+		System.out.println("Edit from master branch!");
 		System.out.println("Edit from release!");
 	}
 }
